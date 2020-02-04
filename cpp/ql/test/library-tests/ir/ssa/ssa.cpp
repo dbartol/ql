7af4dfb580e457a7b6f9b99150a7f943e611d03f
Dave Bartolomeo <dbartol@github.com>
2020-02-04 14:16:26 -0700

    C++: Add tests for performance fallback

--- cpp/ql/test/library-tests/ir/ssa/ssa.cpp
+++ cpp/ql/test/library-tests/ir/ssa/ssa.cpp
@@ -243,3 +243,15 @@ void ExplicitConstructorCalls() {
   Constructible c2 = Constructible(2);
   c2.g();
 }
+
+int PerfFallback(int i) {
+  // The size of this array pushes its "expense" metric past the threshold, which turns all memory
+  // accesses to the array to just access an unknown offset within the array.
+  int a[] = {
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
+    32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 
+    64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75
+  };
+
+  return a[0] + a[5] + a[63];  // The definitions of these loads are not exact due to the performance fallback
+}
